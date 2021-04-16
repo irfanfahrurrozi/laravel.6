f574a7e68f67cc5363edd2d37f63059956c2e83b
Irfan Fahrurrozi <irfanfahrurrozi@gmail.com>
2021-04-16 14:26:22 +0700

    sweet alert

--- resources/views/add-student.blade.php
+++ resources/views/add-student.blade.php
@@ -68,7 +68,14 @@
 
         }
     </script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous"></script>
+        @if(Session::has('student_added'))
+            <script>
+                swal("Great Job!", "{!! Session::get('student_added') !!}","success"),{
+                button:"OK",
+                });
+            </script>
+        @endif
     
 </body>
 </html>
